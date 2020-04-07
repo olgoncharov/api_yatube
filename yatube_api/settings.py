86ca9067307c8f1cc3ad8189ca890347b265c35a
Oleg Goncharov <oleg.gnch@gmail.com>
2020-04-07 03:33:36 +0300

    add urls for api

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -134,8 +134,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
 }
--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls', namespace='v1')),
 ]
 
 
