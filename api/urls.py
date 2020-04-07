4e5659be4857bfae7a42c862b2eb9627d8d7fdd8
Oleg Goncharov <oleg.gnch@gmail.com>
2020-04-07 20:57:25 +0300

    create viewset for post

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
 
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
