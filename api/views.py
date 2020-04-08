4d9b7dd10251fde67c2213a8e53ef0c39b2e1ea9
Oleg Goncharov <oleg.gnch@gmail.com>
2020-04-08 03:28:44 +0300

    create viewset for comments

--- api/views.py
+++ api/views.py
@@ -1,11 +1,31 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
+from .permissions import IsAuthor
+from rest_framework.permissions import IsAuthenticated
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments
+
+    def perform_create(self, serializer):
+        post = self.get_post()
+        serializer.save(author=self.request.user, post=post)
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
